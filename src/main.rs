e200797f78d72125e485b2bc1844dfa6fed8deef
hwankee <devenvy100@gmail.com>
2024-06-11 22:38:22 +0900

    await 비동기 함수 설명
    await가 없으면 비동기함수는 실행되지 않는다.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use mini_redis::{client, Result};
 
+// 이 녀석은 매크로이다.
 #[tokio::main]
 async fn main() -> Result<()> {
     // mini-redis 주소에 대한 연결
+    // 이런 비동기 함수(connect())는 await가 붙지 않으면 절대 실행되지 않는다.
+    // rust에서는... 비동기가 엄청 특이하게 동작한다.
     let mut client = client::connect("127.0.0.1:6379").await?;
 
     // 키, 값 설정
